92a8cb5e792820ccd634a51bd95ec7da1c8f8695
Mehdi Asadi <mehdiasadi63@gmail.com>
2024-12-29 15:22:10 +0330

    update price

--- products.js
+++ products.js
@@ -1,19 +1,19 @@
 export const productsData = [
 	{
 		id: 1,
-		title: 'queen panel bed',
+		title: 'Queen panel bed',
 		price: 10.99,
 		imageUrl: './images/product-1.jpeg',
 	},
 	{
 		id: 2,
-		title: 'saheb panel bed',
+		title: 'Mehdi panel bed',
 		price: 14.99,
 		imageUrl: './images/product-2.jpeg',
 	},
 	{
 		id: 3,
-		title: 'folan panel bed',
+		title: 'King panel bed',
 		price: 7.99,
 		imageUrl: './images/product-3.jpeg',
 	},
@@ -25,25 +25,25 @@ export const productsData = [
 	},
 	{
 		id: 5,
-		title: 'fridge',
+		title: 'Kitchen',
 		price: 4.99,
 		imageUrl: './images/product-5.jpeg',
 	},
 	{
 		id: 6,
-		title: 'fridge',
+		title: 'Fridge',
 		price: 23.99,
 		imageUrl: './images/product-6.jpeg',
 	},
 	{
 		id: 7,
-		title: 'dresser',
+		title: 'Dresser',
 		price: 42.99,
 		imageUrl: './images/product-7.jpeg',
 	},
 	{
 		id: 8,
-		title: 'couch',
+		title: 'Couch',
 		price: 24.99,
 		imageUrl: './images/product-8.jpeg',
 	},
